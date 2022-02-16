647357d7273fc16bcd537df70848d5bb629d8c5e
Zwerg93 <mapouget15160@gmail.com>
2022-02-16 06:19:31 +0100

    commit

--- pc-part-shop-client/src/app/components/part-list/part-list.component.ts
+++ pc-part-shop-client/src/app/components/part-list/part-list.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-//import {HttpClient} from "@angular/common/http";
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-part-list',
@@ -9,32 +8,24 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 })
 
 export class PartListComponent implements OnInit {
-  public itemList: { Storage: [], PSU: [], GraphicsCard: [], Motherboard: [], CPU: [], Case: [],RAM:[] } = { Storage: [], PSU: [], GraphicsCard: [], Motherboard: [], CPU: [], Case: [],RAM:[] };
+  public itemList: { [key: string]: { [key: string]: any }[] } = {}; //, PSU: {}[], GraphicsCard: {}[], Motherboard: {}[], CPU: {}[], Case: {}[],RAM: {}[] } = { Storage: [], PSU: [], GraphicsCard: [], Motherboard: [], CPU: [], Case: [],RAM:[] };
 
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
+    // this.getPartList()
   }
 
   getPartList() {
 
-
-
     this.http.get('http://localhost:8080/part')
       .subscribe((response: any) => {
-        this.itemList.Case = response.Case;
-        this.itemList.CPU = response.CPU;
-        this.itemList.GraphicsCard = response.GraphicsCard;
-        this.itemList.Storage = response.Storage;
-        this.itemList.RAM = response.RAM;
-        this.itemList.PSU = response.PSU;
-        this.itemList.Motherboard = response.Motherboard;
-        console.log(this.itemList);
-
+        this.itemList = response;
+        console.log(response)
       });
 
-    return this.itemList;
+    //return this.itemList;
   }
 }
